db22e1bb94d49c489a8a9e54a29bbfd667be40f7
Cenk Kılıç <cenk@kilic.dev>
2020-03-13 22:02:32 +0100

    expanded react rules

--- react-typescript.js
+++ react-typescript.js
@@ -13,9 +13,39 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-unused-vars': 'off',
     'no-unused-vars': 'off',
+    indent: 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     'react/jsx-indent': [ 'error', 2 ],
     'react/jsx-indent-props': [ 'error', 2 ],
-    'react/jsx-curly-newline': [ 'error', 'consistent' ]
+    'react/jsx-curly-newline': [ 'error', 'consistent' ],
+    'react/jsx-fragments': [ 'error', 'element' ],
+    'react/require-render-return': 'error',
+    'react/self-closing-comp': [ 'error', {
+      component: true,
+      html: true
+    } ],
+    'react/jsx-tag-spacing': [ 'error', {
+      closingSlash: 'never',
+      beforeSelfClosing: 'always',
+      afterOpening: 'never',
+      beforeClosing: 'never'
+    } ],
+    'react/jsx-closing-tag-location': 'error',
+    'react/jsx-curly-brace-presence': [ 'error', { props: 'always', children: 'never'} ],
+    'react/jsx-equals-spacing': [ 'error', 'never' ],
+    'react/jsx-first-prop-new-line': [ 'error', 'multiline' ],
+    'react/jsx-props-no-multi-spaces': 'error',
+    'react/jsx-space-before-closing': [ 'error', 'always' ],
+    'react/jsx-wrap-multilines': [ 'error',
+      {
+        declaration: 'parens-new-line',
+        assignment: 'parens-new-line',
+        return: 'parens-new-line',
+        arrow: 'parens-new-line',
+        condition: 'ignore',
+        logical: 'ignore',
+        prop: 'ignore'
+      } ]
+
   }
 }
--- typescript.js
+++ typescript.js
@@ -8,7 +8,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended'
   ],
   parserOptions: {
-    project: './tsconfig.json'
+    project: 'tsconfig.json'
   },
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'error',
