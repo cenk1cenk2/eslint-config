4d0f800414ee9fbdce459f72f4f404bfecb78c82
Cenk Kılıç <cenk@kilic.dev>
2020-03-13 23:50:48 +0100

    trying to fix parser

--- react-typescript.js
+++ react-typescript.js
@@ -3,8 +3,8 @@ module.exports = {
     '@cenk1cenk2/eslint-config/typescript'
   ],
   plugins: [ 'react' ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
     ecmaFeatures: {
       jsx: true
     },
